9a1ec6d442e057263648c576b85ea4438ca9521c
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-20 20:39:01 +0000

    d

--- dc.py
+++ dc.py
@@ -8,9 +8,7 @@ GPIO.setup(pwmPin, GPIO.OUT)
 pwm = GPIO.PWM(pwmPin, 2) 
 pwm.start(0)
 
-
 try:
-  while True:
     for i in range(100): #increase i to 100
       d = 100-i #defines d to decrease as i increases
       pwm.ChangeDutyCycle(d) #decreases duty cycle with d
