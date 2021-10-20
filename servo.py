017fd95700f8ab2017be4e0c2a7aa8e451da1f4f
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-20 20:08:10 +0000

    d

--- servo.py
+++ servo.py
@@ -13,8 +13,8 @@ GPIO.setup(pwmPin, GPIO.OUT)
 
 # set min & max % duty cycles (5 and 10 are default values, but play
 # around to find optimum values for your motor)
-dcMin = 3
-dcMax = 15
+dcMin = 2
+dcMax = 14
 
 pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm.start(0)
