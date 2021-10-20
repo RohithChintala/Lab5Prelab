7126a1040781c2c760ff79c52994b930eecc5ed9
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-20 20:21:45 +0000

    d

--- servo.py
+++ servo.py
@@ -24,7 +24,7 @@ try:
     for dc in range(dcMin,dcMax):
       pwm.ChangeDutyCycle(dc)
       print(dc)
-      time.sleep(0.5)
+      time.sleep(0.02)
 except KeyboardInterrupt:
   print("closing")
 GPIO.cleanup()
